d6c02dd6fc492f23a425d860a02e9ea5c0cbc078
Nathan Brackett <ntb12076@uga.edu>
2019-05-26 17:32:13 -0400

    forgot to add some new files

---
+++ public/javascripts/popup.js
@@ -0,0 +1,32 @@
+/* makes text a popup on hover of a another element
+    params - hoverid (id of desired trigger element)
+           - popupid (id of desired popup text)
+*/
+function makePopup(hoverid, popupid) {
+    //adding listeners for mouseover, mouseout of hovered object
+    document.getElementById(hoverid).addEventListener("mouseover", showPopup);
+    document.getElementById(hoverid).addEventListener("mouseout", closePopup);
+
+    // shows the element identified by popup id (on hoverid element mouse over)
+    function showPopup() {
+        //showing the text
+        document.getElementById(popupid).style.display = "block";
+        
+        //expanding the div to fit the text
+        document.getElementById(popupid).parentElement.style.width = "644px";
+        
+        // if the parent is floated right, float left, or vice versa
+        if(document.getElementById(popupid).parentElement.style.float == "right") {
+            document.getElementById(popupid).style.float = "left";
+        } else {
+            document.getElementById(popupid).style.float = "right";
+        } // if-else
+        
+    } // showPopup
+
+    // closes the element identified by popup id (on hoverid element mouse out)
+    function closePopup() {
+        document.getElementById(popupid).style.display = "none";
+        document.getElementById(popupid).parentElement.style.width = "322px";
+    } // closePopup
+} // makePopup
