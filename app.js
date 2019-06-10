1d06770965ab7f6b1bcd8d40856972201b59b43c
Alexander Proschek <alexander.proschek@gmail.com>
2019-06-10 12:05:39 -0400

    Changed the public folder from /public to /

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(logger('dev'));
 app.use(express.json());
 //app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/public', express.static('public'));
+app.use(express.static('public'));
 
 //app.use('/', indexRouter);
 app.use('/users', usersRouter);
