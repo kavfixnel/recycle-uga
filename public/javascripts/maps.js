f6da0d0954b921f069ffdf68e1041ba4796299a3
Nathan Brackett <nbrackett15@gmail.com>
2019-07-03 11:48:47 -0400

    Added infoWindows to mapPage with recycling centers in Athens

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -1,62 +1,90 @@
-//initializes the map and calls for recycling plants in athens
-function initMap() {
-    //creates map and sets center, zoom
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 33.944895,lng: -83.376212}, 
-        zoom: 13});
+function createMap() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoicmVjeWNsZXVnYSIsImEiOiJjanhuZDRrengwZGhzM2NxZGYyczN0cmtxIn0.90gL17oBS0iAABYgzydKtQ';
 
-    //initializes infowindow
-    infoWindow = new google.maps.InfoWindow();
-
-    // request variable for searching for recycling plants
-    var request = {
-        query: 'recycling center',
-        fields: ['name', 'geometry'],
-        location: {lat: 33.944895,lng: -83.376212},
-        radius: 1000
-        //locationbias: {point: {lat: 33.944895,lng: -83.376212}}
-    };
-
-    //service to make request
-    var service = new google.maps.places.PlacesService(map);
-
-    //search for recycling plants, if successful, create markers
-    //for each result
-    service.textSearch(request, function(results, status) {
-        if(status === google.maps.places.PlacesServiceStatus.OK) {
-            for(var i = 0; i < results.length; i++) {
-                createMarker(results[i]);
-            } // for each location
-
-        } // if status is ok
-    });
-} // initMap
-
-//creates a marker given a location
-function createMarker(place) {
-
-    // make marker object
-    var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location,
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [-83.415, 33.948],
+        zoom: 11.5
     });
 
-    // shows infoWindow on hover
-    google.maps.event.addListener(marker, 'mouseover', function(){
-        // takes 'USA' off of addresses
-        var shortenedAddress = place.formatted_address.substring(0, place.formatted_address.length - 5);
-        
-        // handles case where instead of USA, address said 'United States'
-        if(place.name === "CMC Recycling") 
-            shortenedAddress = shortenedAddress.substring(0, shortenedAddress.length - 10);
-        
-        //sets the infoWindow text to match the marker and places marker on map
-        infoWindow.setContent("<b>" + place.name + "</b><br>" + shortenedAddress);
-        infoWindow.open(map, this);
-    });
+    map.on("load", function() {
+        map.loadImage("https://i.imgur.com/MK4NUzI.png", function (error, image) {
+            if(error) throw error;
+            map.addImage("custom-marker", image);
+            map.addLayer({
+                id: "markers",
+                type: "symbol",
+                source: {
+                    type: "geojson",
+                    data: {
+                        type: "FeatureCollection",
+                        features: [ // SET OF MARKERS TO GO ON MAP
+                            { // marker 1
+                                type: 'Feature',
+                                properties: {
+                                    description: "<strong>Athens-Clarke County Recycling Divison</strong><br>" +
+                                    "725 Hancock Industrial Way, Athens, GA 30605<br>"
+                                },
+                                geometry: { // geometry of this point
+                                    type: "Point",
+                                    coordinates: [-83.335210, 33.967170]
+                                } 
+                            }, { // marker 2
+                                type: 'Feature',
+                                properties: {
+                                    description: "<strong>CMC Recycling</strong><br>" + 
+                                    "1570, 590 Old Hull Rd, Athens, GA 30601",
+                                },
+                                geometry: { // geometry of this point
+                                    type: "Point",
+                                    coordinates: [-83.342680, 33.983030]
+                                } 
+                            }, { // marker 3
+                                type: 'Feature',
+                                properties: {
+                                    description: "<strong>CHaRM (Center for Hard to Recycle Materials)</strong><br>" + 
+                                    "1005 College Ave, Athens, GA 30601",
+                                },
+                                geometry: { // geometry of this point
+                                    type: "Point",
+                                    coordinates: [-83.377970, 33.969200]
+                                } 
+                            }, { // marker 4
+                                type: 'Feature',
+                                properties: {
+                                    description: "<strong>Georgia Computer Recycling</strong><br>" + 
+                                    "240 Collins Ind Blvd, Athens, GA 30601",
+                                },
+                                geometry: { // geometry of this point
+                                    type: "Point",
+                                    coordinates: [-83.359660, 33.979100]
+                                } 
+                            }
+                        ] // features (array for 1+)
+                    } // data
+                }, // source information
 
-    // hides infoWindow on mouse leave
-    google.maps.event.addListener(marker, 'mouseout', function(){
-        infoWindow.close();
-    });
-} // createMarker
+                layout: {
+                    "icon-image": "custom-marker"
+                } // layout information
+            }); // map.addLayer
+        }); // map.loadImage
+    }); // map.on (load)
+
+    // for when popups are clicked (all three of the following are for popups)
+    map.on('click', 'markers', function(e) {
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var description = e.features[0].properties.description;
+
+        new mapboxgl.Popup().setLngLat(coordinates).setHTML(description).addTo(map);
+    }); // map.on (click) for infowindow popup
+
+    map.on('mouseenter', 'markers', function(e) {
+        map.getCanvas().style.cursor = 'pointer';
+    }); // map.on (mouseenter) to change mouse to pointer
+
+    map.on('mouseleave', 'markers', function(e) {
+        map.getCanvas().style.cursor = '';
+    }); // map.on (mouseenter) to change mouse back to normal
+} // createMap
