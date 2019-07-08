a5a66dd6c94fe90e103284996d6125f64bdb97f0
Nathan Brackett <nbrackett15@gmail.com>
2019-07-08 12:17:33 -0400

    added slideshow to index, added more pins to map

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -4,8 +4,8 @@ function createMap() {
     var map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
-        center: [-83.415, 33.948],
-        zoom: 11.5
+        center: [-83.400, 33.953],
+        zoom: 11.85
     });
 
     map.on("load", function() {
@@ -34,7 +34,7 @@ function createMap() {
                                 type: 'Feature',
                                 properties: {
                                     description: "<strong>CMC Recycling</strong><br>" + 
-                                    "1570, 590 Old Hull Rd, Athens, GA 30601",
+                                    "1570, 590 Old Hull Rd, Athens, GA 30601"
                                 },
                                 geometry: { // geometry of this point
                                     type: "Point",
@@ -44,7 +44,7 @@ function createMap() {
                                 type: 'Feature',
                                 properties: {
                                     description: "<strong>CHaRM (Center for Hard to Recycle Materials)</strong><br>" + 
-                                    "1005 College Ave, Athens, GA 30601",
+                                    "1005 College Ave, Athens, GA 30601"
                                 },
                                 geometry: { // geometry of this point
                                     type: "Point",
@@ -54,12 +54,32 @@ function createMap() {
                                 type: 'Feature',
                                 properties: {
                                     description: "<strong>Georgia Computer Recycling</strong><br>" + 
-                                    "240 Collins Ind Blvd, Athens, GA 30601",
+                                    "240 Collins Ind Blvd, Athens, GA 30601"
                                 },
                                 geometry: { // geometry of this point
                                     type: "Point",
                                     coordinates: [-83.359660, 33.979100]
                                 } 
+                            }, { // marker 5
+                                type: 'Feature',
+                                properties: {
+                                    description: "<strong>Free I.T. Athens</strong><br>" +
+                                    "2465 Jefferson Rd, Athens, GA 30607"
+                                }, 
+                                geometry: { // geometry of this point
+                                    type: "Point",
+                                    coordinates: [-83.415779, 33.972561]
+                                }
+                            }, { // marker 6
+                                type: 'Feature',
+                                properties: {
+                                    description: "<strong>ecoATM</strong><br>" +
+                                    "191 Alps Rd, Athens, GA 30606 (inside Kroger)"
+                                },
+                                geometry: { // geometry of this point
+                                    type: "Point",
+                                    coordinates: [-83.406769, 33.946789]
+                                }
                             }
                         ] // features (array for 1+)
                     } // data
@@ -74,9 +94,11 @@ function createMap() {
 
     // for when popups are clicked (all three of the following are for popups)
     map.on('click', 'markers', function(e) {
+        // get coordinates and description from pin after click
         var coordinates = e.features[0].geometry.coordinates.slice();
         var description = e.features[0].properties.description;
 
+        // create a new popup, set its location to event location, set its description, and add it to map
         new mapboxgl.Popup().setLngLat(coordinates).setHTML(description).addTo(map);
     }); // map.on (click) for infowindow popup
 
