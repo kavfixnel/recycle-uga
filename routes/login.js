68992bf93b8eec6176c230f1bd6ac972216cd8e1
Alexander Proschek <alexander.proschek@gmail.com>
2019-07-15 10:33:22 -0400

    Added debugging code

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ let express = require('express')
 let router = express.Router()
 let axios = require('axios')
 let crypto = require('crypto')
-let {google} = require('googleapis');
+let { google } = require('googleapis');
 
 let userModel = require('../schemas/user.js')
 
@@ -13,7 +13,7 @@ router.get('/cas', (req, res, next) => {
 });
 
 router.get('/cb', async (req, res) => {
-	if(req.query.ticket == null) {
+	if (req.query.ticket == null) {
 		// There is no ticket avaliable
 		res.redirect('/')
 	} else {
@@ -21,38 +21,38 @@ router.get('/cb', async (req, res) => {
 		try {
 			const url = 'https://cas2.stage.uga.edu/cas/validate?service=https://recycle-uga.herokuapp.com/login/cb&ticket=' + req.query.ticket
 			const response = await axios.get(url)
-    			var ans = response.data.split('\n')
-			if(ans[0] == 'yes') {
+			var ans = response.data.split('\n')
+			if (ans[0] == 'yes') {
 				// Ticket was valid and ans[1] has username
-				var user = await userModel.findOne( { id : ans[1] } )
-				if(!user) {
+				var user = await userModel.findOne({ id: ans[1] })
+				if (!user) {
 					// New user needs to be created
 					var newCookie = random(30)
-					await userModel.create({ id : ans[1], ugaStudent : true, cookie : newCookie, cookieExp: (Date.now() + 86400000) })
-					if(process.env.DEVMODE == 'TRUE') {
+					await userModel.create({ id: ans[1], ugaStudent: true, cookie: newCookie, cookieExp: (Date.now() + 86400000) })
+					if (process.env.DEVMODE == 'TRUE') {
 						console.log('New User created: ' + ans[1])
 						console.log('New cookie issued: ' + newCookie)
 					}
-					res.cookie('sessionCookie', newCookie, {maxAge:86400000})
+					res.cookie('sessionCookie', newCookie, { maxAge: 86400000 })
 				} else {
 					// User already exists
 					// Create new cookie
 					var newCookie = random(30)
 
 					// Update the cookie of the user
-					await userModel.findOneAndUpdate({id:ans[1]}, {$set: {cookie: newCookie, cookieExp: (Date.now() + 86400000)}})
+					await userModel.findOneAndUpdate({ id: ans[1] }, { $set: { cookie: newCookie, cookieExp: (Date.now() + 86400000) } })
 
-					if(process.env.DEVMODE == 'True') {
+					if (process.env.DEVMODE == 'True') {
 						console.log(`New cookie (${newCookie}) issued for ${ans[1]}`)
 					}
 
-					res.cookie('sessionCookie', newCookie, {maxAge:86400000})
+					res.cookie('sessionCookie', newCookie, { maxAge: 86400000 })
 				}
 			}
 		} catch (error) {
 			console.error(error)
 		}
-		
+
 		// Redirect the user to the homepage
 		res.redirect('/').send()
 	}
@@ -64,17 +64,17 @@ router.get('/cb', async (req, res) => {
 
 // Create an OAuth2 Google client
 const oauth2Client = new google.auth.OAuth2(
-  process.env.CLIENTID,
-  process.env.CLIENTSECRET,
-  'https://recycle-uga.herokuapp.com/login/googlecb'
+	process.env.CLIENTID,
+	process.env.CLIENTSECRET,
+	'https://recycle-uga.herokuapp.com/login/googlecb'
 );
 
 // Redirect the users to the Google OAuth page
 router.get('/google', (req, res) => {
 	// Generate a new redirection URL to Googles OAuth network
-	let redirectURL =oauth2Client.generateAuthUrl({
-        	access_type: 'offline',
-        	scope: 'https://www.googleapis.com/auth/userinfo.email'
+	let redirectURL = oauth2Client.generateAuthUrl({
+		access_type: 'offline',
+		scope: 'https://www.googleapis.com/auth/userinfo.email'
 	});
 
 	// Redirect the user to that URL
@@ -83,26 +83,26 @@ router.get('/google', (req, res) => {
 
 // The callback URL for Google OAuth
 router.get('/googlecb', async (req, res) => {
-	const {tokens} = await oauth2Client.getToken(req.query.code)
+	const { tokens } = await oauth2Client.getToken(req.query.code)
 
-	console.log(tokens)
+	console.log('Token: ' + tokens)
 
-	let axiosResponse = await axois('https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token='+tokens)
+	let axiosResponse = await axois('https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=' + tokens)
 
-	console.log(axiosResponse.data)
+	console.log('Response: ' + axiosResponse.data)
 })
 
-function random (howMany, chars) {
-      chars = chars
-        || 'abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789';
-    var rnd = crypto.randomBytes(howMany)
-    var value = new Array(howMany)
-    var len = len = Math.min(256, chars.length)
-    var d = 256 / len
+function random(howMany, chars) {
+	chars = chars
+		|| 'abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789';
+	var rnd = crypto.randomBytes(howMany)
+	var value = new Array(howMany)
+	var len = len = Math.min(256, chars.length)
+	var d = 256 / len
 
-    for (var i = 0; i < howMany; i++) {
-          value[i] = chars[Math.floor(rnd[i] / d)]
-    };
+	for (var i = 0; i < howMany; i++) {
+		value[i] = chars[Math.floor(rnd[i] / d)]
+	};
 
 	return value.join('')
 }
