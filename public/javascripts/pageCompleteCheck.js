128b822a7fd8ba9e8eec314dbfd65718af4c1b18
Nathan Brackett <nbrackett15@gmail.com>
2019-07-16 16:11:14 -0400

    fixed post survey page

--- public/javascripts/pageCompleteCheck.js
+++ public/javascripts/pageCompleteCheck.js
@@ -23,6 +23,33 @@ function checkIfCompletePreSurvey() {
         document.getElementsByClassName("nextPageButton").item(0).classList.remove("disabled");
 } // checkIfCompletePreSurvey
 
+// checks to see if all forms are filled (for preSurveyPage)
+function checkIfCompletePostSurvey() {
+    // counts the total amount of forms completed (if all, should be 8 at end)
+    var finishedFormCount = 0;
+
+    // checking if length of name input is greater than zero
+    if(document.getElementsByName("comments")[0].value.length > 0)
+        finishedFormCount++;
+
+    for(var qCount = 1; qCount <= numQuestions; qCount++) {
+        var currentAnswers = document.getElementsByName("question" + qCount.toString());
+
+        for(var i = 0; i < currentAnswers.length; i++) {
+            if(currentAnswers[i].checked) {
+                finishedFormCount++;
+                break;
+            } // if one of the answer choices is checked
+
+        } // for each answer in the current question
+    } // for each question
+    
+    console.log(finishedFormCount);
+
+    if(finishedFormCount === numForms) 
+        document.getElementsByClassName("nextPageButton").item(0).classList.remove("disabled");
+} // checkIfCompletePostSurvey
+
 // checks if all questions are answered to enable the next page
 // button (for infoPage)
 function checkIfCompleteInfo() {
