ea591c5fd30974365f3645124668009e7af1a834
Nathan Brackett <nbrackett15@gmail.com>
2019-07-22 12:42:43 -0400

    edited post variables for gamePage

--- public/javascripts/pageCompleteCheck.js
+++ public/javascripts/pageCompleteCheck.js
@@ -2,23 +2,24 @@
 function checkIfCompletePreSurvey() {
     // counts the total amount of forms completed (if all, should be 8 at end)
     var finishedFormCount = 0;
-
-    // checking if length of name input is greater than zero
-    if(document.getElementById("name").value.length > 0)
+    
+    if(document.getElementsByName('name')[0].value.length > 0) 
         finishedFormCount++;
-
-    for(var qCount = 1; qCount <= numQuestions; qCount++) {
-        var currentAnswers = document.getElementsByName("question" + qCount.toString());
-
-        for(var i = 0; i < currentAnswers.length; i++) {
-            if(currentAnswers[i].checked) {
-                finishedFormCount++;
-                break;
-            } // if one of the answer choices is checked
-
-        } // for each answer in the current question
-    } // for each question
-
+    
+    if(document.getElementsByName('email')[0].value.length > 0)
+        finishedFormCount++;
+    
+    if(document.getElementsByName('age')[0].value.length > 0)
+        finishedFormCount++;
+    
+    if(document.getElementsByName('why')[0].value.length > 0)
+        finishedFormCount++;
+    
+    for(var i = 0; i < numQuestions; i++) {
+        if(i > 7)
+            
+    } // for each question w/o text form
+        
     if(finishedFormCount === numForms) 
         document.getElementsByClassName("nextPageButton").item(0).classList.remove("disabled");
 } // checkIfCompletePreSurvey
